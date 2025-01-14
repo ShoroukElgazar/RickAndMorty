f20144c176de46cc53a6b6f7fbb7d7fd974b9c35
Shorouk.Mohamed <shorouk.mahmoud@etisalat.com>
2025-01-14 17:17:53 +0200

    add missing dependencies in characterDetails SPM

--- RickAndMorty/Packages/Modules/CharacterDetails/CharactersDetails/Package.swift
+++ RickAndMorty/Packages/Modules/CharacterDetails/CharactersDetails/Package.swift
@@ -2,7 +2,10 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-let characterDetailsInterface = "CharacterDetailsInterface"
+private let characterDetailsInterface = "CharacterDetailsInterface"
+private let commonUI = "CommonUI"
+private let utilities = "Utilities"
+private let commonModels = "CommonModels"
 
 let package = Package(
     name: "CharactersDetails",
@@ -14,7 +17,11 @@ let package = Package(
             targets: ["CharactersDetails"]),
     ],
     dependencies: [
-        .package(name: characterDetailsInterface, path: "../\(characterDetailsInterface)")],
+        .package(name: characterDetailsInterface, path: "../\(characterDetailsInterface)"),
+        .package(name: commonUI, path: "../../\(commonUI)"),
+        .package(name: utilities, path: "../../\(utilities)"),
+        .package(name: commonModels, path: "../../\(commonModels)")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -22,6 +29,9 @@ let package = Package(
             name: "CharactersDetails",
         dependencies: [
         .product(name: characterDetailsInterface, package: characterDetailsInterface),
+        .product(name: commonUI, package: commonUI),
+        .product(name: utilities, package: utilities),
+        .product(name: commonModels, package: commonModels)
         ]),
         .testTarget(
             name: "CharactersDetailsTests",
